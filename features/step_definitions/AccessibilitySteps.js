e3a49314468af0a06a303a724ddd8a8a34bb65ba
Joseph Richie <joseph.richie@qumu.com>
2024-02-20 17:44:52 +0530

    Added HTML report support

--- features/step_definitions/AccessibilitySteps.js
+++ features/step_definitions/AccessibilitySteps.js
@@ -1,6 +1,7 @@
 import { Given, When, Then } from '@cucumber/cucumber';
 import AxeBuilder from '@axe-core/webdriverjs';
 import fs from 'fs';
+import { createHtmlReport } from 'axe-html-reporter';
 
 Then('I do an accessibility check for the {string} Page', async function(PageType){
 
@@ -10,8 +11,10 @@ Then('I do an accessibility check for the {string} Page', async function(PageTyp
     if (result.violations.length > 0){
         const ViolationsList = JSON.stringify(result.violations)
         fs.writeFileSync('./features/reports/'+PageType+'PageViolations.json', ViolationsList)
+        createHtmlReport({ results: { violations: result.violations }, options :{outputDir: './features/reports/', reportFileName: PageType + 'PageViolations.html'} })
     }
     else{
         fs.writeFileSync('./features/reports/'+PageType+'PageViolations.json', "No Violations Found")
     }
+
 })
